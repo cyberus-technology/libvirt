a7d915d955770230946d17e41f102aed92096952
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2025-07-04 15:50:54 +0200

    xxx fix unused warning
    On-behalf-of: SAP philipp.schuster@sap.com
    Signed-off-by: Philipp Schuster <philipp.schuster@cyberus-technology.de>

--- src/ch/ch_driver.c
+++ src/ch/ch_driver.c
@@ -432,7 +432,7 @@ chDomainDefineXML(virConnectPtr conn, const char *xml)
 
 static int
 chDomainUndefineFlags(virDomainPtr dom,
-                      unsigned int flags)
+                      unsigned int)
 {
     virCHDriver *driver = dom->conn->privateData;
     virDomainObj *vm;
@@ -3015,6 +3015,7 @@ chDomainAttachDeviceLive(virDomainObj *vm,
     return ret;
 }
 
+#if 0
 static int
 chDomainAttachDeviceConfig(virDomainDef *vmdef,
                            virDomainDeviceDef *dev,
@@ -3099,6 +3100,7 @@ chDomainAttachDeviceConfig(virDomainDef *vmdef,
 
     return 0;
 }
+#endif
 
 static void
 chDomainAttachDeviceLiveAndConfigHomogenize(const virDomainDeviceDef *devConf,
@@ -3136,7 +3138,9 @@ chDomainAttachDeviceLiveAndConfig(virDomainObj *vm,
     // virCHDomainObjPrivate *priv = vm->privateData;
     unsigned int parse_flags = VIR_DOMAIN_DEF_PARSE_INACTIVE |
                                VIR_DOMAIN_DEF_PARSE_ABI_UPDATE;
+#if 0
     virObjectEvent *event = NULL;
+#endif
     g_autoptr(virDomainDeviceDef) devLive = NULL;
     g_autoptr(virDomainDef) vmdef = NULL;
     g_autoptr(virCHDriverConfig) cfg = NULL;
@@ -3436,7 +3440,9 @@ chDomainDetachDeviceLiveAndConfig(virCHDriver *driver,
                                   unsigned int flags)
 {
     // qemuDomainObjPrivate *priv = vm->privateData;
+#if 0
     virObjectEvent *event = NULL;
+#endif
     g_autoptr(virCHDriverConfig) cfg = NULL;
     g_autoptr(virDomainDeviceDef) dev_config = NULL;
     g_autoptr(virDomainDeviceDef) dev_live = NULL;
