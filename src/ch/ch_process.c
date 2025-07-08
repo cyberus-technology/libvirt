641388d7f9d559a6dbcb4da8fa608eb645cf807d
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2025-07-08 11:04:10 +0200

    ch: align virCHProcessStart and virCHProcessStartRestore
    Further, the doc of virCHProcessPrepareHost says
    that virCHProcessPrepareDomain must be called before.
    
    On-behalf-of: SAP philipp.schuster@sap.com
    Signed-off-by: Philipp Schuster <philipp.schuster@cyberus-technology.de>

--- src/ch/ch_process.c
+++ src/ch/ch_process.c
@@ -1335,6 +1335,10 @@ virCHProcessStartRestore(virCHDriver *driver, virDomainObj *vm, const char *from
     }
     logfile = domainLogContextGetWriteFD(logCtxt);
 
+    if (virCHProcessPrepareDomain(vm) < 0) {
+        return -1;
+    }
+
     if (virCHProcessPrepareHost(driver, vm) < 0)
         return -1;
 
