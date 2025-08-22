e77a003ba0b9ce56543ce05c40f651368b9033ff
Stefan Kober <stefan.kober@cyberus-technology.de>
2025-08-22 13:30:12 +0200

    ch: avoid null ptr exceptions

--- src/ch/ch_driver.c
+++ src/ch/ch_driver.c
@@ -4062,7 +4062,12 @@ chStateStop(void)
 static int
 chStateShutdownPrepare(void)
 {
-    VIR_WARN("in chStateShutdownPrepare\n");
+    VIR_WARN("in chStateShutdownPrepare");
+    // We can reach this function before chStateInitialize is completed.
+    if (!ch_driver) {
+        return 0;
+    }
+
     virThreadPoolStop(ch_driver->workerPool);
     return 0;
 }
@@ -4071,6 +4076,10 @@ static int
 chStateShutdownWait(void)
 {
     VIR_WARN("in chStateShutdownWait\n");
+    // We can reach this function before chStateInitialize is completed.
+    if (!ch_driver) {
+        return 0;
+    }
     /* virDomainObjListForEach(ch_driver->domains, false, */
                             /* qemuDomainObjStopWorkerIter, NULL); */
     virThreadPoolDrain(ch_driver->workerPool);
