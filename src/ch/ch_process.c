825d62a7addd7457471ed7da56aebb4e1d096d2b
Stefan Kober <stefan.kober@cyberus-technology.de>
2025-09-18 13:03:52 +0200

    ch: issue warning on double shutdown

--- src/ch/ch_process.c
+++ src/ch/ch_process.c
@@ -1266,6 +1266,7 @@ virCHProcessStopOrKill(virCHDriver *driver,
     // A shutdown might be already on-going e.g. because an event triggered it.
     // Do not do it twice in this case.
     if (g_atomic_int_exchange(&priv->shutdown_done, 1) == 1) {
+        VIR_WARN("Shutdown already in progress or done");
         return 0;
     }
 
