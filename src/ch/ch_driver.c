2be6e4cd27ee335fbebac32b07d7d850d1534fae
Stefan Kober <stefan.kober@cyberus-technology.de>
2025-11-13 10:19:14 +0100

    ch: fix memory leak: ch_driver
    On-behalf-of: SAP stefan.kober@sap.com
    Signed-off-by: Stefan Kober <stefan.kober@cyberus-technology.de>

--- src/ch/ch_driver.c
+++ src/ch/ch_driver.c
@@ -4292,6 +4292,8 @@ chStateShutdownWait(void)
     /* virDomainObjListForEach(ch_driver->domains, false, */
                             /* qemuDomainObjStopWorkerIter, NULL); */
     virThreadPoolDrain(ch_driver->workerPool);
+
+    VIR_FREE(ch_driver);
     return 0;
 }
 
