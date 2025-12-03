2524253c7cc84ecbe081502e72dfdde19418d40f
Thomas Prescher <thomas.prescher@cyberus-technology.de>
2025-12-03 16:31:55 +0100

    ch: change job type for processMonitorEOFEvent
    This is necessary because otherwise, the VM destruction
    will race with processMonitorEOFEvent if the sender VMM
    crashes during a live migration.

--- src/ch/ch_driver.c
+++ src/ch/ch_driver.c
@@ -1611,8 +1611,9 @@ processMonitorEOFEvent(virCHDriver *driver,
         return;
     }
 
-    if (virDomainObjBeginJob(vm, VIR_JOB_DESTROY) < 0)
+    if (virDomainObjBeginJob(vm, VIR_JOB_MODIFY) < 0) {
         return;
+    }
 
     if (!virDomainObjIsActive(vm)) {
         VIR_DEBUG("Domain %p '%s' is not active, ignoring EOF",
