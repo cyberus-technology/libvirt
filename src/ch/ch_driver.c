5d6b5ec0f9469058afa12a496a74d1184d845779
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2025-12-16 07:29:39 +0100

    ch-driver: more robust error handling in chDomainMigratePrepare3
    Signed-off-by: Philipp Schuster <philipp.schuster@cyberus-technology.de>
    On-behalf-of: SAP philipp.schuster@sap.com

--- src/ch/ch_driver.c
+++ src/ch/ch_driver.c
@@ -2861,15 +2861,20 @@ chDomainMigratePrepare3(virConnectPtr dconn,
     DBG("%p %s %u %p %p %s %p %lu %s %s",
         dconn, cookiein, cookieinlen, cookieout, cookieoutlen, uri_in, uri_out, flags, dname, dom_xml);
 
-    if (virDomainMigratePrepare3EnsureACL(dconn, def) < 0)
-        return -1;
+    if (virDomainMigratePrepare3EnsureACL(dconn, def) < 0) {
+        rc = -1;
+        goto cleanup;
+    }
 
-    if (!(def = chMigrationAnyPrepareDef(driver, dom_xml, dname)))
-        return -1;
+    if (!(def = chMigrationAnyPrepareDef(driver, dom_xml, dname))) {
+        rc = -1;
+        goto cleanup;
+    }
 
     VIR_INFO("Got DomainDef prepared successfully");
 
     if (virPortAllocatorAcquire(driver->migrationPorts, &port) < 0) {
+        rc = -1;
         goto cleanup;
     }
     VIR_DEBUG("Got port %i", port);
@@ -2894,17 +2899,20 @@ chDomainMigratePrepare3(virConnectPtr dconn,
     {
         virReportError(VIR_ERR_INTERNAL_ERROR, "%s",
                        _("Could not add Domain Obj to List"));
+        rc = -1;
         goto cleanup;
     }
 
     if (chMigrationJobStart(vm, VIR_ASYNC_JOB_MIGRATION_IN) < 0) {
         virReportError(VIR_ERR_INTERNAL_ERROR, "%s",
                        _("Could not begin async migration job"));
+        rc = -1;
         goto cleanup;
     }
 
     if (virCHProcessInit(driver, vm) < 0) {
         DBG("Could not init process");
+        rc = -1;
         goto cleanup;
     }
 
@@ -2949,6 +2957,7 @@ chDomainMigratePrepare3(virConnectPtr dconn,
                             args) < 0) {
         virReportError(VIR_ERR_OPERATION_FAILED, "%s",
                        _("Failed to create thread for receiving migration data"));
+        rc = -1;
         goto cleanup;
     }
 
@@ -2975,7 +2984,9 @@ chDomainMigratePrepare3(virConnectPtr dconn,
 
 
  cleanup:
-    virDomainObjEndAPI(&vm);
+    if (vm != NULL) {
+        virDomainObjEndAPI(&vm);
+    }
     return rc;
 }
 
