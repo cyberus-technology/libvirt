1d2cc54a470a5dbb372f34d14d255f6060a65077
Paul Kroeher <paul.kroeher@cyberus-technology.de>
2026-04-23 10:07:11 +0200

    ci: windows live migration with previous version
    Add a new windows live migration test with
    cpu profiles and using the previous CHV version
    on one bare metal machine.
    The test performs a live migration from the host
    with the current CHV version to the host with
    the previous version of CHV.
    
    Signed-off-by: Paul Kroeher <paul.kroeher@cyberus-technology.de>
    On-behalf-of: SAP paul.kroeher@sap.com

--- local_tests/run_chv_profile_prepare_chv.sh
+++ local_tests/run_chv_profile_prepare_chv.sh
@@ -6,8 +6,17 @@ set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 BASE_DIR="${SCRIPT_DIR}/.."
+HOSTNAME=$(hostname)
+CMD="${1:-current}"
 
 cd ${BASE_DIR}
-nix build .\#packages.x86_64-linux.cloud-hypervisor
+
+if [ "$CMD" == "prev" ]; then
+  echo "Build cloud hypervisor in the previous release version on ${HOSTNAME}"
+  nix build .\#packages.x86_64-linux.cloud-hypervisor-prev
+else
+  echo "Build cloud hypervisor in the current version on ${HOSTNAME}"
+  nix build .\#packages.x86_64-linux.cloud-hypervisor
+fi
 
 ${BASE_DIR}/result/bin/cloud-hypervisor --version
