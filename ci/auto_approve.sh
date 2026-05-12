6c433ba94881633272874362273e2645f219a6f1
Paul Kroeher <paul.kroeher@cyberus-technology.de>
2026-05-12 14:02:43 +0200

    ci: fix auto approve step
    Auto approve script checks if a merge request
    contains only one commit with a flake.lock update.
    If the MR contains other changes the MR
    will not be auto approved and the pipeline
    will not fail.
    
    Signed-off-by: Paul Kroeher <paul.kroeher@cyberus-technology.de>
    On-behalf-of: SAP paul.kroeher@sap.com

--- ci/auto_approve.sh
+++ ci/auto_approve.sh
@@ -1,10 +1,25 @@
 #!/usr/bin/env bash
 
-# approval 1
-curl --fail --request POST --header "PRIVATE-TOKEN: ${AUTO_APPROVE_1}" ${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/merge_requests/${CI_MERGE_REQUEST_IID}/approve
+TARGET_BRANCH=${CI_MERGE_REQUEST_TARGET_BRANCH_NAME:-$CI_DEFAULT_BRANCH}
+echo "TARGET_BRANCH=$TARGET_BRANCH"
+nix run nixpkgs#gitlint -- --commits origin/$TARGET_BRANCH.. -C .gitlint_auto_approve
 
-# approval 2
-curl --fail --request POST --header "PRIVATE-TOKEN: ${AUTO_APPROVE_2}" ${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/merge_requests/${CI_MERGE_REQUEST_IID}/approve
+ELIGIBLE_AUTO_APPROVE=$?
 
-# merge
-curl --fail --request PUT --header "PRIVATE-TOKEN: ${AUTO_APPROVE_1}" --data "merge_when_pipeline_succeeds=true" ${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/merge_requests/${CI_MERGE_REQUEST_IID}/merge
+if [ $ELIGIBLE_AUTO_APPROVE -eq 0 ]; then
+  echo "this merge request is eligible for a flake bump auto approve and merge"
+
+  # approval 1
+  curl --fail --request POST --header "PRIVATE-TOKEN: ${AUTO_APPROVE_1}" ${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/merge_requests/${CI_MERGE_REQUEST_IID}/approve
+
+  # approval 2
+  curl --fail --request POST --header "PRIVATE-TOKEN: ${AUTO_APPROVE_2}" ${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/merge_requests/${CI_MERGE_REQUEST_IID}/approve
+
+  # merge
+  curl --fail --request PUT --header "PRIVATE-TOKEN: ${AUTO_APPROVE_1}" --data "merge_when_pipeline_succeeds=true" ${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/merge_requests/${CI_MERGE_REQUEST_IID}/merge
+
+else
+  echo "this merge request will not be automatically approved."
+fi
+
+exit 0
