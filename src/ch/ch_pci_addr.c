11dc965d7ccb0aaff72fa1d06633056851989f14
Stefan Kober <stefan.kober@cyberus-technology.de>
2026-05-18 15:37:29 +0200

    ch_pci_addr.c: style
    Signed-off-by: Stefan Kober <stefan.kober@cyberus-technology.de>
    On-behalf-of: SAP stefan.kober@sap.com

--- src/ch/ch_pci_addr.c
+++ src/ch/ch_pci_addr.c
@@ -1,5 +1,5 @@
 #include <config.h>
- 
+
 #include <unistd.h>
 #include <fcntl.h>
 #include <poll.h>
@@ -22,7 +22,7 @@
 #define VIR_FROM_THIS VIR_FROM_CH
 VIR_LOG_INIT("ch.ch_pci_addr");
 
-// Helper to collect `virDomainDeviceInfo`s that need to allocate a completely new PCI device ID. 
+// Helper to collect `virDomainDeviceInfo`s that need to allocate a completely new PCI device ID.
 typedef struct _dynamicAddressQueue {
     // Current length of the queue
     size_t size;
@@ -37,7 +37,7 @@ typedef struct _dynamicAddressQueue {
  * @queue: Out; Pointer to the newly allocated queue
  *
  * Returns -1 if memory allocation fails, otherwise 0 to indicate success
- */ 
+ */
 static int new_addr_queue(dynamicAddressQueue **queue) {
     dynamicAddressQueue *new_queue = g_new0 (dynamicAddressQueue, 1);
     if (new_queue) {
@@ -46,7 +46,7 @@ static int new_addr_queue(dynamicAddressQueue **queue) {
         new_queue->devInfos = g_new0(virDomainDeviceInfo*, new_queue->size);
         *queue = new_queue;
         return 0;
-    } 
+    }
     return -1;
 }
 
@@ -56,7 +56,7 @@ static int new_addr_queue(dynamicAddressQueue **queue) {
  * @devInfo: virDomainDeviceInfo to add to the queue
  *
  *  Returns 0 on success and -1 if the queue grows and reallocation fails.
- */ 
+ */
 static int add_info_to_queue(dynamicAddressQueue *queue, virDomainDeviceInfo *devInfo) {
     if (queue->nextIdx >= queue->size) {
         queue->size *= 2;
@@ -72,7 +72,7 @@ static int add_info_to_queue(dynamicAddressQueue *queue, virDomainDeviceInfo *de
 /**
  * Frees all memory owned by the queue struct and sets queue to NULL on success.
  * @queue: Pointer to a pointer of the queue to free. Will be overwritten with 0
- */ 
+ */
 static void free_queue(dynamicAddressQueue **queue) {
     g_free((*queue)->devInfos);
     g_free(*queue);
@@ -95,7 +95,7 @@ static virDomainPCIAddressSet* chDomainPCIAddressSetCreate(virDomainDef *def) {
     }
 
     if (def->ncontrollers > 0) {
-        virReportError(VIR_ERR_INVALID_ARG, 
+        virReportError(VIR_ERR_INVALID_ARG,
             _("No additional PCI controllers supported right now!"));
         return NULL;
     }
@@ -108,7 +108,7 @@ static virDomainPCIAddressSet* chDomainPCIAddressSetCreate(virDomainDef *def) {
             _("CHV currently does't support more than one PCI bus!"));
         return NULL;
     }
-    
+
     // We need to allocate some representation of the PCI bus for libvirt to manage devices
     if (!(addrs = virDomainPCIAddressSetAlloc(nbuses, VIR_PCI_ADDRESS_EXTENSION_NONE)))
         return NULL;
@@ -123,7 +123,7 @@ static virDomainPCIAddressSet* chDomainPCIAddressSetCreate(virDomainDef *def) {
  * @devInfo: Pointer to the DeviceInfo of the device to reserve a PCI slot ID for
  * @queue: Queue of DeviceInfos for which a slot ID is reserved later
  *
- * Returns: 0 on success, -1 in case of error 
+ * Returns: 0 on success, -1 in case of error
  */
 static int chReserveOrQueueForPciSlotId(virDomainPCIAddressSet *addrSet,
                                         virDomainDeviceInfo *devInfo,
@@ -136,7 +136,7 @@ static int chReserveOrQueueForPciSlotId(virDomainPCIAddressSet *addrSet,
         // ... add it to the queue for later slot id assignment to prevent conflicts
         if (add_info_to_queue(queue, devInfo)) {
             virReportError(VIR_ERR_INTERNAL_ERROR,
-                _("Failed to add device '%s' to the queue for PCI ID assignment!"), 
+                _("Failed to add device '%s' to the queue for PCI ID assignment!"),
                                             devInfo->alias);
             return -1;
         }
@@ -163,7 +163,7 @@ static int chReserveOrQueueForPciSlotId(virDomainPCIAddressSet *addrSet,
             virReportError(VIR_ERR_INTERNAL_ERROR,
                 _("Couldn't reserve PCI slot %d from config of device with alias '%s'!"),
                                             devInfo->addr.pci.slot, devInfo->alias);
-            return -1;    
+            return -1;
         }
         DBG("Assigned Slot %d to device '%s", devInfo->addr.pci.slot, devInfo->alias);
     }
@@ -174,10 +174,10 @@ static int chReserveOrQueueForPciSlotId(virDomainPCIAddressSet *addrSet,
  * Initialize all RNG devices in the domain with an PCI slot ID. Adds a default RNG device if given an empty list of
  * devices.
  *
- * CHV adds a default RNG device if none is provided in the VM config. We mimic this behavior by creating 
- * an RNG device with CHV defaults if the domain does not contain an RNG device. This function allocates a new 
+ * CHV adds a default RNG device if none is provided in the VM config. We mimic this behavior by creating
+ * an RNG device with CHV defaults if the domain does not contain an RNG device. This function allocates a new
  * array in case an empty one is provided.
- * Device without a slot ID given in the XML are queued for later ID assignment and therefore not initialized by this 
+ * Device without a slot ID given in the XML are queued for later ID assignment and therefore not initialized by this
  * function.
  *
  * @addrSet: PciAddressSet to reserve the slot IDs from
@@ -185,7 +185,7 @@ static int chReserveOrQueueForPciSlotId(virDomainPCIAddressSet *addrSet,
  * @deviceDevs: Pointer to the array of RNG devices of a domain
  * @queue: Queue of DeviceInfos for which a slot ID is reserved later
  *
- * Returns: 0 on success, -1 in case of error 
+ * Returns: 0 on success, -1 in case of error
  */
 static int chInitRngVirtioPciDevices(virDomainPCIAddressSet *addrSet,
                                      size_t *numDefs,
@@ -196,7 +196,7 @@ static int chInitRngVirtioPciDevices(virDomainPCIAddressSet *addrSet,
     // Assign an address to all RNG devices
     for (idx = 0; idx < *numDefs; ++idx) {
         virDomainRNGDef *rng = (*deviceDefs)[idx];
-        if (chReserveOrQueueForPciSlotId(addrSet, &rng->info, queue)) 
+        if (chReserveOrQueueForPciSlotId(addrSet, &rng->info, queue))
             return -1;
     }
     return 0;
@@ -205,7 +205,7 @@ static int chInitRngVirtioPciDevices(virDomainPCIAddressSet *addrSet,
 /**
  * Initialize all network devices in the domain with an PCI slot ID or places them in a queue for later assignment.
  *
- * Device without a slot ID given in the XML are queued for later ID assignment and therefore not initialized by this 
+ * Device without a slot ID given in the XML are queued for later ID assignment and therefore not initialized by this
  * function.
  *
  * @addrSet: PciAddressSet to reserve the slot IDs from
@@ -213,7 +213,7 @@ static int chInitRngVirtioPciDevices(virDomainPCIAddressSet *addrSet,
  * @deviceDevs: Array of network devices definitions of a domain
  * @queue: Queue of DeviceInfos for which a slot ID is reserved later
  *
- * Returns: 0 on success, -1 in case of error 
+ * Returns: 0 on success, -1 in case of error
  */
 static int chInitNetworkVirtioPciDevices(virDomainPCIAddressSet *addrSet,
                                          size_t numDefs,
@@ -236,7 +236,7 @@ static int chInitNetworkVirtioPciDevices(virDomainPCIAddressSet *addrSet,
 /**
  * Initialize all disk devices in the domain with an PCI slot ID or places them in a queue for later assignment.
  *
- * Device without a slot ID given in the XML are queued for later ID assignment and therefore not initialized by this 
+ * Device without a slot ID given in the XML are queued for later ID assignment and therefore not initialized by this
  * function.
  *
  * @addrSet: PciAddressSet to reserve the slot IDs from
@@ -244,7 +244,7 @@ static int chInitNetworkVirtioPciDevices(virDomainPCIAddressSet *addrSet,
  * @deviceDevs: Array of disk devices definitions of a domain
  * @queue: Queue of DeviceInfos for which a slot ID is reserved later
  *
- * Returns: 0 on success, -1 in case of error 
+ * Returns: 0 on success, -1 in case of error
  */
 static int chInitDiskVirtioPciDevices(virDomainPCIAddressSet *addrSet,
                                       size_t numDefs,
@@ -270,7 +270,7 @@ static int chInitDiskVirtioPciDevices(virDomainPCIAddressSet *addrSet,
  * @addrSet: PciAddrSet to reserve the PCI BDFs from
  * @queue: Queue of devices for which a PCI BDF will be reserved
  *
- * Returns: 0 on success, -1 in case of error 
+ * Returns: 0 on success, -1 in case of error
  */
 static int chReserveDynamicPciIds(virDomainPCIAddressSet *addrSet, dynamicAddressQueue *queue) {
     for (size_t idx = 0; idx < queue->nextIdx; ++idx) {
@@ -290,14 +290,14 @@ static int chReserveDynamicPciIds(virDomainPCIAddressSet *addrSet, dynamicAddres
 }
 
 /* Initializes all PCI devices in the configuration with PCI bus addresses.
- * 
+ *
  * First allocates all PCI slot IDs given by the XML. Then allocates slot IDs for all devices without a fixed slot ID.
- * 
+ *
  * Succeeds if after successfully assigning an address to each device.
  *
  * @dom: Domain object for which all PCI devices are initialized with an PCI address.
- * 
- * 0 indicates success, -1 failure 
+ *
+ * 0 indicates success, -1 failure
  */
 static int chInitPciDevices(virDomainDef *def, virDomainPCIAddressSet *addrSet) {
     dynamicAddressQueue* queue = NULL;
@@ -307,9 +307,9 @@ static int chInitPciDevices(virDomainDef *def, virDomainPCIAddressSet *addrSet) 
     /* Either allocate PCI slot IDs for all devices of all device classes if their XML contains a fixed slot id
     * or place them in a queue to later assign fixes slot IDs. This is necessary as there is no order with respect
     * to fixed IDs. Moreover, there is no guarantee that even if there was an order in one device class, there would be
-    * fixed slot ID in a class that is later initialized. So we need a global queue of all devices without a fixed slot 
+    * fixed slot ID in a class that is later initialized. So we need a global queue of all devices without a fixed slot
     * ID.
-    */ 
+    */
     if (chInitRngVirtioPciDevices(addrSet, &def->nrngs, &def->rngs, queue)) {
         rc = -1;
         VIR_ERROR("%s:%d: Failed to attach Virtio RNG devices.", __FILE_NAME__, __LINE__);
@@ -338,7 +338,7 @@ static int chInitPciDevices(virDomainDef *def, virDomainPCIAddressSet *addrSet) 
 }
 
 /*
- * Assigns PCI Addresses to all devices in a definition and persists them if 
+ * Assigns PCI Addresses to all devices in a definition and persists them if
  * there is a domain object.
  */
 int chAssignPciAddresses(virDomainDef *def, virDomainObj *obj) {
@@ -352,7 +352,7 @@ int chAssignPciAddresses(virDomainDef *def, virDomainObj *obj) {
             addr_set = priv->pciAddrSet;
         } else {
             // An object without privateData initialized seems like a bad thing (shouldn't exists).
-            VIR_ERROR("%s:%d: Cannot allocate PCI addresses! Domain object supplied but private Data not initialized!", 
+            VIR_ERROR("%s:%d: Cannot allocate PCI addresses! Domain object supplied but private Data not initialized!",
                       __FILE_NAME__,
                       __LINE__);
             return -1;
@@ -395,10 +395,10 @@ int chAssignPciAddresses(virDomainDef *def, virDomainObj *obj) {
 
 /* Ensures that a hotplugged PCI devices has a associated address.
  *
- * Performs all necessary configuration for a hotplugable PCI device. 
+ * Performs all necessary configuration for a hotplugable PCI device.
  * Succeeds after successfully assigning an address to the device.
- * 
- * 0 indicates success, -1 failure 
+ *
+ * 0 indicates success, -1 failure
  */
 int chEnsurePciAddress(virDomainObj *obj, virDomainDeviceDef *dev) {
     virCHDomainObjPrivate *priv = obj->privateData;
