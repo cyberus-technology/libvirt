7e120b3ec58f4799c33dabf469ac5429802a2419
Paul Kroeher <paul.kroeher@cyberus-technology.de>
2026-05-21 08:42:02 +0200

    ci: set tmpdir for cpu profiles test suite
    Signed-off-by: Paul Kroeher <paul.kroeher@cyberus-technology.de>
    
    On-behalf-of: SAP paul.kroeher@sap.com

--- local_tests/run_cpu_profile_test.sh
+++ local_tests/run_cpu_profile_test.sh
@@ -31,6 +31,10 @@ nix build .\#tests.x86_64-linux.cpu_profiles.passthru.no_port_forwarding.driver
 ########################
 # run test suite
 ########################
+XDG_RUNTIME_DIR=$(mktemp -d)
+export XDG_RUNTIME_DIR
+trap 'rm -rf "$XDG_RUNTIME_DIR"' EXIT
 ./local_tests/get_child_pids.sh & # run in background
 echo ">>> Run test suite"
 nix run .\#tests.x86_64-linux.cpu_profiles.passthru.no_port_forwarding.driver
+rm -rf $XDG_RUNTIME_DIR
